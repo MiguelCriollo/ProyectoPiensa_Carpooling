9b74b6bf4df0044a2a39571db8b7b499738ed7ff
Miguell Criollo <macriollo.2@sudamericano.edu.ec>
2023-11-24 07:49:52 -0500

    Folders of Controller, Models, Services, Repositories

--- src/main/kotlin/com/example/Aventon/models/User.kt
+++ src/main/kotlin/com/example/Aventon/models/User.kt
@@ -1,6 +1,9 @@
 package com.example.Aventon.models
 
 import jakarta.persistence.*
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+
 @Entity
 @Table(name = "users")
 class User {
@@ -8,13 +11,18 @@ class User {
     @Id
     @Column(updatable = false)
     var id: Long? = null
+    @NotBlank(message="Campo obligatorio") //validate
     @Column(name="first_name")
     var firstName: String? = null
+    @NotBlank(message="Campo obligatorio") //validate
     @Column(name="last_name")
     var lastName: String?= null
+    @NotBlank(message="Campo obligatorio") //validate
     @Column(name="cell_number")
     var cellNumber: String? =null
+    @Email(message="Correo Valido???Noze")
     var email:String?=null
+    @NotBlank(message="Campo obligatorio") //validate
     var password:String?=null
     @Column(name="id_driver")
     var idDriver:Boolean?=false
